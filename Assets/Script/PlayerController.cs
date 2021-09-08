8f912448b5aa70a27bae2541159ffa3923a40083
NagataKentaro <86968899+NagataKentaro@users.noreply.github.com>
2021-09-09 02:36:15 +0900

    MoveMode
    動き方を変更しました。

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,34 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rigidbody;
+
     [SerializeField]
     private float speed = 10.0f;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(new Vector3(speed * Time.deltaTime * -1, 0, 0));
+            rigidbody.AddForce(new Vector3(speed * Time.deltaTime * -1, 0, 0));
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            rigidbody.AddForce(new Vector3(speed * Time.deltaTime, 0, 0));
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+            rigidbody.AddForce(new Vector3(0, 0, speed * Time.deltaTime));
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime * -1));
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(new Vector3(0, 20.0f * Time.deltaTime, 0));
+            rigidbody.AddForce(new Vector3(0, 0, speed * Time.deltaTime * -1));
         }
     }
 }
