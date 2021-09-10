069a80496ed11f85e7519bb8a10a7de19b34819a
okamoto-kouki <38881241+okamoto-kouki@users.noreply.github.com>
2021-09-11 06:40:44 +0900

    Revert "Merge pull request #49 from InnovaGameCreate/feature/Controlpanel３"
    This reverts commit 8dfcd32fd425af6731b62952b5c9d52f4c67a2af, reversing
    changes made to 6bded184bb49e13706c5c1e98ffe1ecbf747eab5.

--- Assets/Script/SelectPanel/DoorStop.cs
+++ Assets/Script/SelectPanel/DoorStop.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DoorStop : MonoBehaviour
 {
-    //O2incidentのクリア条件用のbool
-    bool O2_pargeClear = false;
-    
     [SerializeField]
     GameObject door_O2tank;
 
     [SerializeField]
-    GameObject door_Storage;
+    GameObject door_Strage;
 
     [SerializeField]
     GameObject door_Engineroom;
@@ -24,44 +20,21 @@ public class DoorStop : MonoBehaviour
     GameObject door_Livingroom;
 
     Button_identify script;
-
-    OpenDoor DoorScript;
     // Start is called before the first frame update
     void Start()
     {
-          script = GameObject.Find("MainPanel").GetComponent<Button_identify>();
-          
+        
     }
-    //他の場所から選択している部屋の名前を持ってくるようにする
-    public void selectRoom(string roomName)
-    {
 
-        if (script.O2tank == true && roomName == "02")
-        {
-            DoorScript = door_O2tank.GetComponent<OpenDoor>();
-            O2_pargeClear = true;
-        }
-        else if (script.Storage == true && roomName == "storage")
-            DoorScript = door_Storage.GetComponent<OpenDoor>();
-
-        else if (script.Engine == true && roomName == "Engine")
-            DoorScript = door_Storage.GetComponent<OpenDoor>();
-
-        else if (script.Coffee == true && roomName == "Coffee")
-            DoorScript = door_Storage.GetComponent<OpenDoor>();
-
-        else if (script.Living == true && roomName == "Living")
-            DoorScript = door_Storage.GetComponent<OpenDoor>();
-
-
-    }
     // Update is called once per frame
-    public void OnClick()
+    public void ButtonPushed()
     {
-        Debug.Log(script);
-        if (DoorScript == null) return;
-        DoorScript.CanOpenDoor = false;
-
-       
+        Debug.Log("AA");
+        if (script.O2tank == true)
+        {
+            script = GameObject.Find("mainPanel").GetComponent<Button_identify>();
+            door_O2tank.GetComponent<OpenDoor>().enabled = false;
+            Debug.Log("O2タンクが閉鎖");
+        }
     }
 }
