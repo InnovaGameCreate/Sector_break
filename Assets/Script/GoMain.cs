07c85237c3ed034a0d7ecfb8f0cb268b1c33a884
SatouNanoha <c612101309@tokushima-u.ac.jp>
2021-09-12 14:31:30 +0900

    シーン移動
    タイトルからspaceシーン挟んでゲーム画面に飛ぶようにしました

--- Assets/Script/GoMain.cs
+++ Assets/Script/GoMain.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
 public class GoMain : MonoBehaviour
 {
-    public void SelectStart()
-    {
-        SceneManager.LoadScene("Stage_CameraMove");
-    }
+    private float step_time;    // 経過時間カウント用
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        step_time = 0.0f;       // 経過時間初期化
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 経過時間をカウント
+        step_time += Time.deltaTime;
+
+        // 3秒後に画面遷移（scene2へ移動）
+        if (step_time >= 4.0f)
+        {
+            SceneManager.LoadScene("Stage_CameraMove");
+        }
     }
 }
