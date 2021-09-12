f299fc44f98fb42388d5cec20e16ae6cf56f367f
NagataKentaro <86968899+NagataKentaro@users.noreply.github.com>
2021-09-12 15:41:36 +0900

    電力計算
    電力計算できるようになりました

--- Assets/Script/Electric/electricData.cs
+++ Assets/Script/Electric/electricData.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class electricData : MonoBehaviour
 {
     public float electric;
+    private float electricSupply = 75.0f;
+    private float electricComsume = 95.0f;
     // Start is called before the first frame update
     void Start()
     {
         electric = 100.0f;
+        StartCoroutine("electricNumber");
+        StartCoroutine("elctricCounter");
     }
 
     public void EngineClick()
@@ -18,14 +22,33 @@ public class electricData : MonoBehaviour
 
     public void solarfix()
     {
-        electric += 10;
+        electricSupply += 10;
     }
 
     private void Update()
     {
-        if (electric < 0)
+        //electric += 0.1f * (electricSupply - electricComsume);
+    }
+
+    IEnumerator electricNumber()
+    {
+        for (electric = 100; electric > 0;)
         {
-            Debug.Log("YouDead");
+            electric += 0.03f * (electricSupply  - electricComsume);
+            yield return new WaitForSeconds(1.0f);
         }
     }
+
+    IEnumerator elctricCounter()
+    {
+        yield return new WaitForSeconds(150.0f);
+        electricSupply -= 30;
+        yield return new WaitForSeconds(150.0f);
+        electricSupply -= 30;
+        yield return new WaitForSeconds(150.0f);
+        electricSupply -= 30;
+        yield break;
+    }
+
+
 }
