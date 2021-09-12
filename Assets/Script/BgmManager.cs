d153398d015d1a7a61641d86a99056374882e2d7
syoutyan212 <82939122+syoutyan212@users.noreply.github.com>
2021-09-12 19:29:24 +0900

    buildしました

--- Assets/Script/BgmManager.cs
+++ Assets/Script/BgmManager.cs
@@ -8,23 +8,6 @@ public class BgmManager : MonoBehaviour
 {
     public Slider BGMslider;
     AudioSource audioSource;
-    public static BgmManager Instance   //DontDestroyï°êªñhé~Ç…égóp
-    {
-        get; private set;
-    }
-
-    void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-
-        if (SceneManager.GetActiveScene().name != "Starting")
-            DontDestroyOnLoad(this.gameObject);
-    }
 
     // Start is called before the first frame update
     void Start()
