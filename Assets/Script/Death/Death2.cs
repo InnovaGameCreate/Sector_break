aabb6003a51adb58fc778ad43fe96da2f07dd0e7
NagataKentaro <86968899+NagataKentaro@users.noreply.github.com>
2021-09-12 16:45:30 +0900

    death2
    デス２を実装しました

---
+++ Assets/Script/Death/Death2.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death2 : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+    [SerializeField]
+    GameObject ItemBar;
+    [SerializeField]
+    GameObject Explosion;
+    private bool dead = true;
+
+    public bool Dead { get => dead; set => dead = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Dead == false)
+        {
+            StartCoroutine("DeathTwo");
+        }
+
+    }
+    IEnumerator DeathTwo()
+    {
+        yield return new WaitForSeconds(0f);
+        Player.GetComponent<PlayerController>().enabled = false;
+        ItemBar.SetActive(false);
+        yield return new WaitForSeconds(5f);
+        Explosion.SetActive(true);//ƒAƒjƒƒVƒ‡ƒ“‚ÌÄ¶
+        yield return new WaitForSeconds(6f);
+        SceneManager.LoadScene("Stage_CameraMove");
+
+    }
+}
--- Assets/Script/Electric/electricData.cs
+++ Assets/Script/Electric/electricData.cs
@@ -7,9 +7,12 @@ public class electricData : MonoBehaviour
     public float electric;
     private float electricSupply = 75.0f;
     private float electricComsume = 95.0f;
+
+    Death2 death2;
     // Start is called before the first frame update
     void Start()
     {
+
         electric = 100.0f;
         StartCoroutine("electricNumber");
         StartCoroutine("elctricCounter");
@@ -37,6 +40,14 @@ public class electricData : MonoBehaviour
             electric += 0.03f * (electricSupply  - electricComsume);
             yield return new WaitForSeconds(1.0f);
         }
+        if(electric < 0)
+        {
+            Debug.Log("0");
+            death2 = GameObject.Find("GameSystem").GetComponent<Death2>();
+            death2.Dead = false;
+
+        }
+
     }
 
     IEnumerator elctricCounter()
